d55628260f4210ac357d8e5c6408020eee8b36b5
dageron93 <vladb93games@gmail.com>
2024-01-12 18:10:19 +0200

    Add waves mechanic

--- Assets/__Game/Scripts/Controllers/SpawnerController.cs
+++ Assets/__Game/Scripts/Controllers/SpawnerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace GDTestWork
@@ -12,9 +13,10 @@ namespace GDTestWork
 
     public List<EnemySpawner> EnemySpawners { get; set; } = new();
 
-    private List<EnemyController> _spawnedEnemies = new();
+    private readonly List<EnemyController> _spawnedEnemies = new();
     private Dictionary<EnemyController, ObjectPool<EnemyController>> _enemyPools;
     private Dictionary<EnemySpawnWaveSO, int> _spawnedEnemiesCount;
+    private int _removedEnemiesCount;
 
     private void Awake()
     {
@@ -27,6 +29,7 @@ namespace GDTestWork
 
       InitializeEnemyPools();
       StartCoroutine(SpawnEnemies());
+      StartCoroutine(CheckAndStartNewWave());
     }
 
     public void AddEnemySpawner(EnemySpawner enemySpawner)
@@ -42,6 +45,7 @@ namespace GDTestWork
     public void RemoveSpawnedEnemy(EnemyController enemy)
     {
       _spawnedEnemies.Remove(enemy);
+      _removedEnemiesCount++;
     }
 
     private void InitializeEnemyPools()
@@ -88,6 +92,41 @@ namespace GDTestWork
               _spawnedEnemiesCount[waveSO]++;
             }
           }
+        }
+      }
+    }
+
+    private IEnumerator CheckAndStartNewWave()
+    {
+      while (true)
+      {
+        yield return null;
+
+        foreach (var waveSO in _spawnedEnemiesCount.Keys.ToList())
+        {
+          if (_removedEnemiesCount >= waveSO.EnemiesLimit)
+          {
+            yield return new WaitForSeconds(3f);
+
+            _removedEnemiesCount = 0;
+            _spawnedEnemiesCount.Remove(waveSO);
+
+            var index = enemySpawnWaveSOs.IndexOf(waveSO);
+            var nextIndex = (index + 1) % enemySpawnWaveSOs.Count;
+            var nextWaveSO = enemySpawnWaveSOs[nextIndex];
+
+            _spawnedEnemiesCount.Add(nextWaveSO, 0);
+
+            Debug.Log($"Starting new wave: {nextWaveSO.name}");
+          }
+        }
+
+        if (_spawnedEnemiesCount.Count == 0)
+        {
+          _removedEnemiesCount = 0;
+          _spawnedEnemiesCount.Add(enemySpawnWaveSOs[0], 0);
+
+          Debug.Log("Restarting from the first wave");
         }
       }
     }
